dd4a0c090c97b6787008601f386d6fad0856879c
liyuntao <lyt3253445223@outlook.com>
2022-04-27 15:06:58 +0800

    feat: 增加官网路由

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,9 @@
 /*
  * @Author: 朱占伟
- * @LastEditors: 朱占伟
+ * @LastEditors: 李云涛
  * @description: 路由控制层
  * @Date: 2022-04-22 15:07:10
- * @LastEditTime: 2022-04-27 11:00:58
+ * @LastEditTime: 2022-04-27 15:05:38
  */
 
 const router = require("koa-router")();
@@ -24,9 +24,134 @@ function Router(App) {
 
     //console.log("控制层:", res)
 
-    return ctx.render("home", {
+    return ctx.render("home/home", {
       name: '住在我',
       url: 2222
+    })
+  })
+
+
+  //套餐购买
+  //购买页-package
+  router.get("/package", async (ctx) => {
+
+    /**数据请求 */
+
+
+
+    return ctx.render("package/package", {
+      name: 'This is package',
+      data: 2222,
+    })
+  })
+
+  //购买页wx支付-package-Wxpay
+  router.get("/package/wxPay", async (ctx) => {
+
+    /**数据请求 */
+
+
+
+    return ctx.render("package/wxPay", {
+      name: 'This is Wxpay',
+      data: 'Please pay 300 yuan',
+    })
+  })
+
+
+  //提取API-getApi
+  router.get("/getApi", async (ctx) => {
+
+    /**数据请求 */
+
+
+
+    return ctx.render("getApi/getApi", {
+      name: 'This is getApi',
+      data: 2222,
+    })
+  })
+
+
+  //帮助中心
+  //帮助中心-helpCenter
+  router.get("/helpCenter", async (ctx) => {
+
+    /**数据请求 */
+
+
+
+    return ctx.render("help/helpCenter", {
+      name: 'This is helpCenter',
+      data: 2222,
+    })
+  })
+
+  //帮助中心-helpCenter-tab-page
+  router.get("/helpCenter/:alias/:page", async (ctx) => {
+
+    /**数据请求 */
+    const {alias, page} = ctx.request.params
+
+
+    return ctx.render("help/helpCenter", {
+      name: `This is helpCenter + ${alias} + ${page}`,
+      data: `333`,
+    })
+  })
+
+  //帮助中心-helpCenter-details
+  router.get("/helpDetails/:id", async (ctx) => {
+
+    /**数据请求 */
+    const {id} = ctx.request.params
+
+
+    return ctx.render("help/helpDetails", {
+      name: `This is ${id} article`,
+      data: 2222,
+    })
+  })
+
+
+  //企业服务-firmsServer
+  router.get("/firmsServer", async (ctx) => {
+
+    /**数据请求 */
+  
+  
+  
+    return ctx.render("firmsServer/firmsServer", {
+      name: 'This is firmsServer',
+      data: 2222,
+    })
+  })
+
+
+  //用户操作中心
+  //用户总页面-login-index
+  router.get("/login/index", async (ctx) => {
+
+    /**数据请求 */
+  
+  
+  
+    return ctx.render("login/index", {
+      name: 'This is main login',
+      data: 2222,
+    })
+  })
+
+  //用户协议-user-protocol
+  router.get("/user/protocol", async (ctx) => {
+
+    /**数据请求 */
+  
+  
+  
+    return ctx.render("user/protocol", {
+      name: 'This is protocol window',
+      data: 2222,
     })
   })
 
@@ -38,6 +163,15 @@ function Router(App) {
     ctx.response.body = htmls
   })
 
+
+  /** 404 */
+  router.get("/404", async (ctx) => {
+
+    return ctx.render("error/404", {
+      name: 'This is 404',
+    })
+    
+  })
 
 
   App.use(router.routes()); //作用：启动路由
