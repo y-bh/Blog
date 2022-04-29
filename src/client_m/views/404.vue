94c220a01efac55c5a8de6767fd515d7e56753c7
zhuzhanwei <zhanwei.zhu@xiaoxitech.com>
2022-04-29 12:28:19 +0800

    新增

--- src/client_m/views/404.vue
+++ src/client_m/views/404.vue
@@ -24,9 +24,7 @@
 //import topNav from "@/components/navMenu/topNav";
 
 export default {
-    components: {
-        //topNav
-    },
+
     data() {
 
         return {
@@ -34,107 +32,9 @@ export default {
             timeClock: ''
         }
     },
-   async mounted() {
-        // this.jumpInterval();
-        this.jumpTime = setTimeout(this.jumpInterval, 1000);
-    },
-    methods: {
-        jumpInterval(){
-           this.timeClock = setInterval(() => {
-                if(this.jumpTime > 0){
-                    this.jumpTime--;
-                } else {
-                    this.jumpIndex();
-                }
-
-            }, 1000);
-        },
-        jumpIndex(){
-            clearInterval(this.timeClock);
-            this.$message({
-                showClose: true,
-                message: '即将回到首页',
-                duration: 2000,
-                type: 'success',
-                onClose: function(){
-                    location.href = '/index/index';
-                }
-            });
-        },
-
-        back() {
-            this.$message({
-                showClose: true,
-                message: '即将回到首页',
-                duration: 1000,
-                type: 'success',
-                onClose: function(){
-                    location.href = '/index/index';
-                }
-            });
-            // location.href = "/index/index.html"
-        },
-        last() {
-            this.$router.go(-1);
-        },
-    },
-    // watch:{
-    //     jumpTime:
-    // }
 
 }
 </script>
 <style lang="scss" scoped>
-.page-container {
-    font-size: 20px;
-    text-align: center;
-    width: 100%;
-    height: 100%;
-    background: url("/my/images/index/tq404.png") no-repeat;
-    margin: 0;
-    background-size: 100% 100%;
 
-    p {
-        margin: 0;
-    }
-
-    .tip1 {
-        color: #333333;
-        padding: 33% 0 1% 0;
-    }
-
-    .tip2 {
-        color: #666666;
-    }
-
-    .click {
-        width: 27%;
-        margin: 30px auto 0;
-
-        .back {
-            width: 220px;
-            height: 60px;
-            background: #3E6AE2;
-            border-radius: 4px;
-            font-size: 24px;
-            color: #FFFFFF;
-            border: none;
-        }
-
-        .last {
-            width: 220px;
-            height: 60px;
-            border: 1px solid #3E6AE2;
-            border-radius: 4px;
-            font-size: 24px;
-            color: #63A2FF;
-            background: transparent;
-        }
-
-        .last:hover {
-            background: transparent;
-        }
-    }
-
-}
 </style>
--- src/client_m/views/home/index.vue
+++ src/client_m/views/home/index.vue
@@ -3,16 +3,19 @@
  * @LastEditors: 朱占伟
  * @description: page description
  * @Date: 2022-04-25 17:37:05
- * @LastEditTime: 2022-04-28 17:03:18
+ * @LastEditTime: 2022-04-29 12:10:22
 -->
 <template>
   <div>我是4432</div>
   <el-button type="primary">Primary</el-button>
-  <img src="/manager/images/empty.png" />
+  <img src="/manager/images/empty.png" alt="">
+  <div class="bg"></div>
+  <div class="bg2"></div>
+  <img src="/manager/images/logo.png" alt="">
 </template>
 
 <script>
-import "assets/images/empty.png";
+
 import { inject } from "vue";
 import { getTest } from "model/index.js";
 import { useStore } from "vuex";
@@ -31,4 +34,18 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.bg{
+  width: 300px;
+  height: 300px;
+  margin: 10px auto;
+  border: 1px solid red;
+ //background: url(assets/images/empty.png);
+}
+.bg2{
+  width: 300px;
+  height: 300px;
+  margin: 10px auto;
+  border: 1px solid red;
+ background: url(assets/images/logo.png);
+}
 </style>
