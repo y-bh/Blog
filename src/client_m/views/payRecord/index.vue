e3efde889d2abf2f55b1b9cc28dd682eeabaa541
chenhaotian <haotian.chen@xiaoxitech.com>
2022-05-14 16:18:24 +0800

    style: 个人中心-购买记录调整

--- src/client_m/views/payRecord/index.vue
+++ src/client_m/views/payRecord/index.vue
@@ -3,12 +3,12 @@
  * @LastEditors: 陈昊天
  * @description: 购买记录
  * @Date: 2022-05-13 15:09:26
- * @LastEditTime: 2022-05-14 15:27:49
+ * @LastEditTime: 2022-05-14 16:16:58
 -->
 <template>
   <div class="container grid">
     <div class="top">
-      <el-form inline>
+      <el-form class="form flex flex-wrap">
         <el-form-item label="订单编号">
           <el-input placeholder="请输入订单编号" v-model="searchForm.orderNo"></el-input>
         </el-form-item>
@@ -28,9 +28,9 @@
         </el-form-item>
       </el-form>
       <div class="btn flex justify-end">
-        <el-button @click="onReset">重置</el-button>
-        <el-button @click="onSearch">查询</el-button>
-        <el-button @click="batchDelete">批量删除</el-button>
+        <el-button class="blue-btn" @click="onReset">重置</el-button>
+        <el-button class="blue-btn" @click="onSearch">查询</el-button>
+        <el-button class="blue-btn" @click="batchDelete">批量删除</el-button>
       </div>
     </div>
     <div class="bottom">
@@ -44,7 +44,7 @@
         }"
         @selection-change="handleSelectionChange"
       >
-        <el-table-column type="selection" width="55" />
+        <el-table-column type="selection" />
         <el-table-column 
           prop="orderNo" 
           label="订单编号" 
@@ -115,6 +115,7 @@
         <el-table-column 
           label="创建|支付时间" 
           align="center"
+          sortable
         >
           <template #default="{ row }">
             <div class="box flex-center flex-column">
@@ -128,7 +129,7 @@
           align="center"
         >
           <template #default="{ row }">
-            <el-button v-show="row.state === 1" @click="onPay">支付</el-button>
+            <el-button v-show="row.state === 1" @click="onPay" size="small" class="pay-btn">支付</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -214,7 +215,15 @@ export default {
 
     //重置
     const onReset = () => {
-      console.log('重置');
+      state.searchForm = {
+        mealName: '', //套餐名称
+        orderNo: '',  //订单编号
+        pageNum: null,  //分页页码
+        pageSize: null, //分页大小
+        payEnd: null, //支付结束时间
+        payStart: null  //支付开始时间
+      }
+      state.timeList = []
     }
 
     //查询
@@ -292,7 +301,7 @@ export default {
       payTypeMap,
       orderTypeMap,
       stateMap,
-      dateFormat
+      dateFormat,
     }
   }
 }
@@ -302,11 +311,14 @@ export default {
   .container {
     grid-template-rows: 190px 1fr;
     grid-row-gap: 40px;
-  }
-  .top {
-    box-shadow: 0px 0px 20px rgba(208, 224, 255, 0.4);
-    border-radius: 4px;
-    padding: 40px;
+    .top {
+      box-shadow: 0px 0px 20px rgba(208, 224, 255, 0.4);
+      border-radius: 4px;
+      padding: 40px;
+    }
+    .form > .el-form-item {
+      margin-right: 50px;
+    }
   }
   .order_red {
     color: #F8486F;
