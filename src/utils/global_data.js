5166793cdf0f4910c8ab22cfcc1540ab937e1d64
zhuzhanwei <zhanwei.zhu@xiaoxitech.com>
2022-05-16 19:02:41 +0800

    公共头部

--- src/utils/global_data.js
+++ src/utils/global_data.js
@@ -3,7 +3,7 @@
  * @LastEditors: 朱占伟
  * @description: 处理node 上下文全局数据
  * @Date: 2022-05-16 18:06:42
- * @LastEditTime: 2022-05-16 18:27:42
+ * @LastEditTime: 2022-05-16 19:01:01
  */
 
 const appKey = require("config/app.key.config")
@@ -18,6 +18,7 @@ module.exports = function (app) {
     //顶部导航 活动相关数据
     const tabActivity = await renderTab();
     state[appKey.active_tab] = tabActivity
+    state.name = "朱占伟"
     await next()
   });
 }
