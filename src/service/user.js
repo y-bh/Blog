e1f1925e34caab6b7a2beeaf63161603ee54e1bd
zhuzhanwei <zhanwei.zhu@xiaoxitech.com>
2022-05-17 20:39:44 +0800

    注册

--- src/service/user.js
+++ src/service/user.js
@@ -3,7 +3,7 @@
  * @LastEditors: 朱占伟
  * @description: 用户相关服务
  * @Date: 2022-05-17 17:02:00
- * @LastEditTime: 2022-05-17 20:26:16
+ * @LastEditTime: 2022-05-17 20:37:51
  */
 
 
@@ -13,9 +13,18 @@ const { postRegisterDao, postResetDao } = require('dao/user')
 const registerService = async (data = null) => {
   //注册校验
 
-
+  const params = {
+    username: data.userName, //用户名
+    pwd: data.pwd, //密码
+    phone: data.phone, //手机号
+    from: '', //来源
+    did: '', //短链
+    code: data.code, //验证码
+    readProtocol: 1, // 是否阅读协议 0 否 1 是
+  }
+  console.log("注册校验", params)
   // 获取注册数据
-  const res = await postRegisterDao(data)
+  const res = await postRegisterDao(params)
 
   //处理注册数据
   console.log("获取注册数据", res)
@@ -43,7 +52,7 @@ const resetService = async (data = null) => {
 
   //处理注册数据
   console.log("获取注册数据", res)
-   
+
   return res
 
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,97 +3,9 @@
  * @LastEditors: 朱占伟
  * @description: 通信封装
  * @Date: 2022-04-25 10:37:04
- * @LastEditTime: 2022-05-17 20:24:20
+ * @LastEditTime: 2022-05-17 20:38:52
  */
-
-
-
-// let request = require('async-request')
 const appConfig = require("config/app.config")
-
-
-
-
-
-// async function axios(url, data = null, method = 'POST', headers = null) {
-//   let response;
-//   try {
-//     if (!url.includes('http')) {
-//       url = `${appConfig.url}${url}`
-//     }
-//     console.log("java点传参:",url, data)
-//     response = await request(url, {
-//       method,
-//       data,
-//       headers
-//     });
-//   } catch (e) {
-//     console.error('axios:', e)
-//   }
-//   console.log("获取处理结果1:", response)
-//   //处理响应
-//   if (response.statusCode === 200) {
-//     // console.log("获取处理结果2:",response)
-//     return JSON.parse(response.body)
-//   }
-//   return console.error('post', response)
-// }
-
-
-
-
-// async function post(url, data = null, headers = null) {
-//   let response;
-//   try {
-//     if (!url.includes('http')) {
-//       url = `${appConfig.url}${url}`
-//     }
-
-//     console.log("java点传参:",url, JSON.stringify(data))
-//     response = await request(url, {
-//       method: 'POST',
-//       data:{
-//         data:data
-//       },
-//       headers :{
-//         "content-type":"application/json"
-//       },
-//     });
-//   } catch (e) {
-//     console.error('post:', e)
-//   }
-
-//   //处理响应
-//   if (response.statusCode === 200) {
-//     return JSON.parse(response.body)
-//   }
-//   return console.error('post', response)
-// }
-// async function get(url, data = null, headers = null) {
-//   let response;
-//   try {
-//     if (!url.includes('http')) {
-//       url = `${appConfig.url}${url}`
-//     }
-//     response = await request(url, {
-//       method: 'GET',
-//       data,
-//       headers
-//     });
-//   } catch (e) {
-//     console.error('GET:', e)
-//   }
-
-//   //处理响应
-//   if (response.statusCode === 200) {
-//     return JSON.parse(response.body)
-//   }
-//   return console.error('GET', response)
-// }
-
-
-
-
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
