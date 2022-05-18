f48ed49686f38d189bc1776dbe457f2027ffe485
dengxiujie <1161311218@qq.com>
2022-05-18 09:32:39 +0800

    feat: 增加个人用户页面

--- src/client_m/views/userAccount/index.vue
+++ src/client_m/views/userAccount/index.vue
@@ -3,27 +3,169 @@
  * @LastEditors: dengxiujie
  * @description: page description
  * @Date: 2022-04-27 15:04:59
- * @LastEditTime: 2022-04-27 17:26:13
+ * @LastEditTime: 2022-05-17 18:50:55
 -->
 <template>
-  <div>个人账户。。。</div>
+  <div class="userAccount">
+    <div class="account pt-40">
+      <div class="left mb-10">
+        <div class="user-info">
+          <span class="user-name mr-20">用户名</span><span>zhangsan</span>
+          <div class="user-edit ml-30" @click="editPersonInfo">
+            <img src="@/assets/images/edit.png" alt="" />
+          </div>
+        </div>
+        <div v-if="!isHasSale" class="group-btn ml-60">
+          <el-button type="primary">密钥</el-button>
+          <el-button type="warning" class="ml-20">联系销售</el-button>
+        </div>
+      </div>
+      <div class="account-vipInfo">
+        第<span class="orange ml-15 mr-15">20000</span>位会员
+      </div>
+    </div>
+    <!--  -->
+    <div class="operation">
+      <div class="operate-box ml-20 mr-20 mt-20 mb-20">
+        <div class="name"><span>手机号码</span></div>
+        <div class="content mt-20">
+          <div class="number"><span>188****0909</span></div>
+          <div class="group-btn">
+            <el-button type="primary" plain>换绑手机</el-button>
+            <el-button type="warning" plain @click="dialogPwdVisible=true">修改密码</el-button>
+          </div>
+        </div>
+      </div>
+      <div class="operate-box ml-20 mr-20 mt-20 mb-20 balance">
+        <div class="balance-name">
+          <span>账户余额（天启币）</span>
+          <span class="ml-10">
+            <el-tooltip
+              effect="customized"
+              content="1元=1000天启币，账户余额不支持提现"
+              placement="right"
+              class="common-tooltip"
+            >
+              <i class="iconfont icon-xianxing-wenhao"></i>
+            </el-tooltip>
+          </span>
+        </div>
+        <div class="bottom mt-20">
+          <div class="amount"><span>300，987，222</span></div>
+          <div>
+            <el-button type="warning">
+              <span class="pl-20 pr-20">充值</span>
+            </el-button>
+          </div>
+        </div>
+      </div>
+      <div class="operate-box operate-box-big ml-20 mr-20 mt-20 mb-20">
+        <div class="name"><span>实名认证</span></div>
+        <div class="explain mt-20">
+          <span
+            >根据您的身份选择认证类型，实名信息为非公开信息，我司不会向他人透露您的认证信息</span
+          >
+        </div>
+        <div class="group-btn mt-20 alginRight">
+          <el-button type="primary" plain>个人认证</el-button>
+          <el-button class="customTip" type="primary" plain>企业认证</el-button>
+        </div>
+      </div>
+      <!-- 有销售 -->
+      <div
+        v-if="isHasSale"
+        class="operate-box operate-box-big ml-20 mr-20 mt-20 mb-20 saleBox"
+      >
+        <div class="saleInfo">
+          <div class="name"><span>您的专属销售</span></div>
+          <div class="sale-name mt-20">
+            <span>风清扬</span><span class="ml-15">销售经理</span>
+          </div>
+          <div class="telep mt-20">
+            <span>18809098787</span>
+            <span class="copyBtn ml-20">复制</span>
+          </div>
+        </div>
+        <div class="saleWX">
+          <img src="@/assets/images/managerWX.png" alt="销售微信" />
+        </div>
+      </div>
+    </div>
+
+    <!-- 个人资料弹框 -->
+    <PersonalFormDialog
+      :dialogVisible="dialogPersonVisible"
+      @updateDialog="updatePersonDialog"
+    ></PersonalFormDialog>
+
+    <!--修改密码  -->
+    <updatePwdDialog
+      :dialogVisible="dialogPwdVisible"
+      @updateDialog="updatePwdDialog"
+    ></updatePwdDialog>
+  </div>
 </template>
 
 <script>
-import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
+import PersonalFormDialog from "./dialog/PersonalFormDialog";
+import updatePwdDialog from "./dialog/updatePwdDialog";
+import { ref, reactive, toRefs, onBeforeMount, onMounted } from "vue";
 export default {
-  name: '',
-  props:{
+  name: "",
+  components: {
+    PersonalFormDialog,
+    updatePwdDialog,
   },
+  props: {},
   setup() {
-    onBeforeMount(() => {
-    })
-    onMounted(() => {
-    })
+    let isHasSale = ref(false); //默认无销售
+    const dialogPersonVisible = ref(false);
+    const dialogPwdVisible = ref(true);
+
+    onBeforeMount(() => {});
+    onMounted(() => {});
+
+    const editPersonInfo = () => {
+      //编辑个人信息
+      console.log(3333, dialogPersonVisible);
+      dialogPersonVisible.value = true;
+    };
+    const updatePersonDialog = (falg) => {
+      //个人资料
+      console.log(333333, falg);
+      dialogPersonVisible.value = falg;
+    };
+    const updatePwdDialog = (falg) => {
+      //密码
+      dialogPwdVisible.value = falg;
+    };
+
     return {
-    }
-  }
-  };
+      isHasSale,
+      editPersonInfo,
+      dialogPersonVisible,
+      updatePersonDialog,
+      dialogPwdVisible,
+      updatePwdDialog,
+    };
+  },
+};
 </script>
-<style scoped>
+ 
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
+<style  lang="scss">
+.el-popper.is-customized {
+  /* Set padding to ensure the height is 32px */
+  padding: 6px 12px;
+  background: #e8f0ff;
+  color: #4c5664;
+}
+
+.el-popper.is-customized .el-popper__arrow::before {
+  background: #e8f0ff;
+  right: 0;
+}
+
 </style>
