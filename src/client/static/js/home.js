ae6a236b3d750bb6d5c5df260784df458681e4a3
zhuzhanwei <zhanwei.zhu@xiaoxitech.com>
2022-05-24 19:16:46 +0800

    优化动画效果

--- src/client/static/js/home.js
+++ src/client/static/js/home.js
@@ -25,18 +25,6 @@ function queryAll() {
 }
 
 
-
-
-
-$(function () {
-  let ipNumTotal = randomNum(2000000,2999999);
-  let ipNumUpdate = randomNum(7000000,1300000);
-  numDynamic("cityNum", 0, 200, 4, 50)
-  numDynamic("ipTotalNum", 100000, ipNumTotal, 50000, 50)
-  numDynamic("ipNum", 600000, ipNumUpdate, 80000, 50)
-  numDynamic("canUserNum", 8, 99, 2, 50)
-})
-
 //生成从minNum到maxNum的随机数
 function randomNum(minNum, maxNum) {
   switch (arguments.length) {
@@ -52,28 +40,47 @@ function randomNum(minNum, maxNum) {
   }
 }
 
-  /**
-    * 动态数字方法
-    * ID    => 对应ID
-    * speed => 递增速度 
-    * start:开始数字
-    * step：每次递增
-    */
-  function numDynamic(id, start, end, step, speed) {
-    var span = document.getElementById(id);
-    if (start < end) {
-      var i = start;
-      var t = setInterval(function () {
-        i += step; // 设置每次增加的动态数字，可调整
+/**
+  * 动态数字方法
+  * ID    => 对应ID
+  * speed => 递增速度 
+  * start:开始数字
+  * step：每次递增
+  */
+function numDynamic(id, start, end, step, speed) {
+  var span = document.getElementById(id);
+  if (start < end) {
+    var i = start;
+    var t = setInterval(function () {
+      i += step; // 设置每次增加的动态数字，可调整
 
-        if (i >= end) {
-          span.innerText = Number(end).toLocaleString();
-          clearInterval(t);
-        } else {
-          span.innerText = Number(i).toLocaleString();
-        }
-      }, speed);
-    } else {
-      span.innerText = Number(start).toLocaleString();
-    }
+      if (i >= end) {
+        span.innerText = Number(end).toLocaleString();
+        clearInterval(t);
+      } else {
+        span.innerText = Number(i).toLocaleString();
+      }
+    }, speed);
+  } else {
+    span.innerText = Number(start).toLocaleString();
   }
+}
+
+
+
+
+$(function () {
+  if (getCookie && !getCookie('ipNumTotal')) {
+    let ipNumTotal = randomNum(2000000, 2999999);
+    let ipNumUpdate = randomNum(7000000, 1300000);
+    setCookie('ipNumTotal', ipNumTotal, 0.5)
+    setCookie('ipNumUpdate', ipNumUpdate, 0.5)
+  }
+
+
+
+  numDynamic("cityNum", 0, 200, 4, 50)
+  numDynamic("ipTotalNum", 100000, +getCookie('ipNumTotal'), 50000, 50)
+  numDynamic("ipNum", 600000, +getCookie('ipNumUpdate'), 80000, 50)
+  numDynamic("canUserNum", 8, 99, 2, 50)
+})
