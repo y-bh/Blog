f9925377db124a91b11242934124cadaf1c6f5fa
liyuntao <lyt3253445223@outlook.com>
2022-05-24 10:23:04 +0800

    feat: 企业服务下边距

--- .env.local.js
+++ .env.local.js
@@ -1,11 +1,11 @@
 /*
  * @Author: 朱占伟
- * @LastEditors: 秦琛
+ * @LastEditors: liyuntao
  * @description:本地开发环境配置
  * @Date: 2022-05-18 16:41:25
- * @LastEditTime: 2022-05-20 16:46:04
+ * @LastEditTime: 2022-05-24 09:42:53
  */
 
 module.exports = {
-  "API": "http://192.168.10.62:17001"
+  "API": "http://192.168.10.244:17001"
 }
