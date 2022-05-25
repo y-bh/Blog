3d9c5761cabfd48523b8b3780b6e44b5a08bb9bf
zhuzhanwei <zhanwei.zhu@xiaoxitech.com>
2022-05-25 17:45:23 +0800

    页面优化

--- src/client/static/js/home.js
+++ src/client/static/js/home.js
@@ -69,6 +69,51 @@ function numDynamic(id, start, end, step, speed) {
 
 
 
+//首次进入弹出窗
+let event = null
+
+function diaJump() {
+  if(dia_token){
+    window.location.pathname = '/package'
+    return 
+  }
+  window.location.pathname = '/register'
+}
+
+$('.center-dialog-jump').on('click', diaJump)
+
+$('.left-dialog-jump').on('click', diaJump)
+
+function removeClick(s = null) {
+  if(s){
+    $('.center-dialog-jump').off('click')
+    return 
+  }
+  $('.left-dialog-jump').off('click')
+}
+
+function disableScroll() {
+  document.documentElement.style.overflowY = 'hidden'
+}
+if(actt !== 'd'){
+  disableScroll()
+}
+
+function openScroll() {
+  document.documentElement.style.overflowY = 'scroll'
+}
+
+function closeDialog(e, s = null) {
+  if(s){
+    $(`.${s}`).show(0, openScroll)
+  }
+  removeClick(s)
+  $(`.${e}`).remove()
+}
+
+
+
+//初始化
 $(function () {
   if (getCookie && !getCookie('ipNumTotal')) {
     let ipNumTotal = randomNum(2000000, 2999999);
